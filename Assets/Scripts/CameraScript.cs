ddd23b3f6179126f767158d5c670fd508dda6e9a
Rémi GINER <remi.giner@hotmail.fr>
2021-04-17 23:47:43 +0200

    Remi: Adding camera motion

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    [SerializeField] private float yMax = 90f;
+
+    [SerializeField] private float sensitivity = 90f;
+
+    float xAxis = 0f;
+
+    float vMove = 0f;
+
+    void Update()
+    {
+        xAxis = Input.GetAxis("Mouse Y");
+
+        vMove = transform.eulerAngles.x;
+    }
+
+    private void FixedUpdate()
+	{
+        vMove -= xAxis * sensitivity * Time.fixedDeltaTime;
+
+        vMove = Mathf.Clamp(Mathf.Repeat(vMove + 180, 360) - 180, -yMax, yMax);
+
+        transform.localRotation = Quaternion.Euler(vMove, 0f, 0f);
+    }
+}
---
+++ Assets/Scripts/PlayerRotation.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRotation : MonoBehaviour
+{
+    [SerializeField] private float sensitivity = 90f;
+
+    float yAxis = 0f;
+
+    float hMove = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        yAxis = Input.GetAxis("Mouse X");
+
+        hMove = transform.eulerAngles.y;
+    }
+
+    private void FixedUpdate()
+	{
+        hMove += yAxis * sensitivity * Time.fixedDeltaTime;
+
+        transform.rotation = Quaternion.Euler(0f, hMove, 0f);
+    }
+}
